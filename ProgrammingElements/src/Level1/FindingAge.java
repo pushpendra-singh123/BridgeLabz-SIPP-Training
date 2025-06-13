0da8ec0e5c8ba71c77da56c7df8d48e69df14494
pushpendra-singh123 <pushpendra.singh4_cs22@gla.ac.in>
2025-06-13 09:43:56 +0530

    [pushpendra Singh] fix . remove Unwanted files

---
+++ ProgrammingElements/src/Level1/FindingAge.java
@@ -0,0 +1,9 @@
+package Level1;
+
+public class FindingAge {
+    public static void main(String[] args) {   
+	int birthYear = 2000;
+       int assumeYear = 2025;
+       System.out.println("The Current Age of Harry is :"+(assumeYear-birthYear));
+}
+}
---
+++ ProgrammingElements/src/Level1/HandshakeCalculator.java
@@ -0,0 +1,16 @@
+package Level1;
+
+import java.util.Scanner;
+
+public class HandshakeCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter number of students: ");
+        int numberOfStudents = input.nextInt();
+
+        int handshakes = (numberOfStudents * (numberOfStudents - 1)) / 2;
+
+        System.out.println("The maximum number of possible handshakes is " + handshakes);
+    }
+}
