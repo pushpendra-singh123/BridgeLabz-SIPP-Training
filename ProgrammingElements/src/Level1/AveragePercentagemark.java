03c016b782ca7c1efaffac3744d5976146cffe38
pushpendra-singh123 <pushpendra.singh4_cs22@gla.ac.in>
2025-06-26 09:55:36 +0530

    [pushpendra] Refactor

---
+++ ProgrammingElements/src/Level1/AveragePercentagemark.java
@@ -0,0 +1,10 @@
+package Level1;
+
+public class AveragePercentagemark {
+		public static void main(String[] args) {
+			int math = 94;
+			int physics = 95;
+			int chemistry = 96;
+			System.out.println("The Average Percentage Mark is : " + (math+physics + chemistry)/3);
+		}
+}
