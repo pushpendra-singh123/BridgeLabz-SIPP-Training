c7881b2480330e694a2cc889c579ffdbf3550f00
pushpendra-singh123 <pushpendra.singh4_cs22@gla.ac.in>
2025-06-28 09:23:33 +0530

    [pushpendra] Refactor

--- ProgrammingElements/src/Level1/KilometersToMiles.java
+++
@@ -1,9 +0,0 @@
-package Level1;
-
-public class KilometersToMiles {
-      public static void main(String[] args) {
-    	  double kilometer = 10.6;
-    	  double miles = kilometer /1.6;
-    	  System.out.println("The Distance "+kilometer+ " km in miles is "+miles);
-      }
-}
